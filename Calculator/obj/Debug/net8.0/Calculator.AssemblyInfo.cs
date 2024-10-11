6c861c5c24f194ae896f732606488ea8e9584721
fabelkr <fabelkr22@sps-prosek.cz>
2024-10-11 09:41:53 +0200

    update

--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fabbaee79deea96aa7c1314d0ad285dd6496f8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+256ada3afef622cf490cf61248856cc18a4a49a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
