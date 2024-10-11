742b094759eeec58e6fbc7ade31eb720cdb818d8
fabelkr <145014023+fabelkr@users.noreply.github.com>
2024-10-11 09:18:35 +0200

    Add files via upload

---
+++ Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Calculator.Classes;
+
+namespace Calculator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the calculator app.\n");
+            SetCalc calc = new SetCalc();
+            // Logic logic = new Logic();
+
+            calc.Run();
+        }
+    }
+}
+           
+            
+             
+            
