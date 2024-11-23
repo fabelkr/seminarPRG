8695fd5cb785bf9e53daab51c139ceb677cae41a
fabelkr <fabelkr22@sps-prosek.cz>
2024-11-23 11:54:43 +0100

    simon update

---
+++ Battleship/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.ComponentModel;
+using App.lib;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creating class instances
+            GameSettings setGame = new GameSettings();
+
+            setGame.SetNewGame();
+        }
+    }
+}
