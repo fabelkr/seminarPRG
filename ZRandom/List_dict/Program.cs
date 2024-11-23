3a003651874976fd931dd2ae6b04ad8db34d77ea
fabelkr <fabelkr22@sps-prosek.cz>
2024-11-23 11:56:00 +0100

    ships UI

---
+++ ZRandom/List_dict/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using List_dict.Classes;
+
+namespace List_dict
+{
+    class Program
+    {
+        static Atomic atom = new Atomic();
+        static void Main(string[] args)
+        {
+            List<string> nameList = new List<string> {"Karel", "Jozef", "Petr", "Jan", "Jana", "Marie"};
+
+            atom.PrintList(nameList);
+
+            Console.WriteLine("\nRemoving Karel from the list\n");
+
+            nameList.Remove("Karel");
+
+            atom.PrintList(nameList);
+
+            nameList.RemoveAt(3);
+
+            Console.WriteLine("\nRemoving the 4th element from the list\n");
+
+            atom.PrintList(nameList);
+
+            Dictionary<string, string> foodDict = new Dictionary<string, string> {{"Karel", "Buchtičky se šodó"}, {"Jozef", "Salám"}, {"Petr", "Kebab"}, {"Jan", "Řízek"}, {"Jana", "Jitrničky"}, {"Marie", "Hovna s mákem"}};
+
+            foodDict.Add("Pavla", "Knedlík s uzeným");
+
+            foreach (KeyValuePair<string, string> food in foodDict)
+            {
+                Console.WriteLine($"Oblibene jidlo stutenta {food.Key} je {food.Value}");
+            }
+        }
+    }
+}
---
+++ ZRandom/array/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] intArray = { 4, 9, 5, 6, 8 };
+
+            foreach (int i in intArray)
+            {
+                Console.WriteLine(i);
+            }
+
+            // for (int i = 0; i < intArray.Length; i++)
+            // {
+            //     Console.WriteLine(intArray[i]);
+            // }
+
+            float sum = 0;
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                sum += intArray[i];
+            }
+
+            float average = sum / intArray.Length;
+
+            float min = intArray[0];
+            float max = intArray[0];
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                if (intArray[i] < min)
+                {
+                    min = intArray[i];
+                }
+
+                if (intArray[i] > max)
+                {
+                    max = intArray[i];
+                }
+            }
+
+
+            Console.WriteLine( "suma je:" + sum);
+            Console.WriteLine("prumer je:" + average);
+            Console.WriteLine("min je:" + min);
+            Console.WriteLine("max je:" + max);
+
+            Console.WriteLine("zadej hledaný prvek");
+
+            float search = float.Parse(Console.ReadLine());
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                if (intArray[i] == search)
+                {
+                    Console.WriteLine("prvek je na indexu:" + i);
+                    break;
+                }
+            }
+
+            Random random = new Random();
+
+            float[] extendedArray = new float[100];
+
+            for (int i = 0; i < extendedArray.Length; i++)
+            {
+                extendedArray[i] = random.Next(0, 10);
+            }
+
+            int[] count = new int[10];
+
+            foreach (float i in extendedArray)
+            {
+                count[(int)i]++;
+            }
+
+            for (int i = 0; i < count.Length; i++)
+            {
+                Console.WriteLine($"počet{i} = {count[i]}");
+            }
+        }
+    }
+}
