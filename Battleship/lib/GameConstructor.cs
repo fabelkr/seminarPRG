4a90dc883669a219ad67ded13a4040c16b7a6e53
fabelkr <fabelkr22@sps-prosek.cz>
2024-11-25 23:05:28 +0100

    simon update

---
+++ Battleship/lib/GameConstructor.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel;
+
+namespace App.lib
+{
+    public class GameConstructor
+    {
+        public void CreateNewGame()
+        {
+            Console.WriteLine("New game settings have been set.");
+            //TODO: list all settings and confirm the game initialization
+        }
+    }
+}
--- Battleship/obj/Debug/net8.0/Battleship.AssemblyInfo.cs
+++ Battleship/obj/Debug/net8.0/Battleship.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Battleship")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+955d590eda1159f585268cdb0d65d99b164b01a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2e7ecb1557a966b2356fd3e642eea362acbbda2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Battleship")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Battleship")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
