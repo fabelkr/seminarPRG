d7e21076b2d2959544799664ebf0ceae33dcfe03
fabelkr <fabelkr22@sps-prosek.cz>
2024-11-26 14:50:45 +0100

    simon update

--- Battleship/obj/Debug/net8.0/Battleship.AssemblyInfo.cs
+++ Battleship/obj/Debug/net8.0/Battleship.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Battleship")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2e7ecb1557a966b2356fd3e642eea362acbbda2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7ff381ae4ac9b9ba5b8da8a5fe368218dbe33c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Battleship")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Battleship")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
