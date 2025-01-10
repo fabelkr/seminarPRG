6f0dd7248c3927e14373e767de4a6a936f4af1d1
fabelkr <fabelkr22@sps-prosek.cz>
2025-01-10 09:35:07 +0100

    simon update

---
+++ ZRandom/Class_playground/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using App.lib;
+
+namespace App{
+    internal class Program{
+        static void Duel(Player player, Enemy enemy){
+            while (!player.IsDead() && !enemy.IsDead()){
+                enemy.Hurt(player.GetRandomDamage());
+                if (enemy.IsDead()){
+                    Console.WriteLine("Player wins");
+                    break;
+                }
+                player.Hurt(enemy.GetRandomDamage());
+
+                Console.WriteLine("Player health: " + player.GetHealth());
+                Console.WriteLine("Enemy health: " + enemy.GetHealth());
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args){
+            Player player = new Player(100, 10, "Player 1");
+            Enemy enemy = new Enemy(20, 2, 1, "Enemy 1");
+
+            Duel(player, enemy);
+
+            Enemy enemy2 = new Enemy(20, 5, 2, "Enemy 2");
+
+            Duel(player, enemy2);
+        }
+    }
+}
